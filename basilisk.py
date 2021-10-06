9a124750892803bb08046bb7a5a5fe47cffc3415
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2021-10-06 09:09:28 -0400

    Update basilisk.py

--- basilisk.py
+++ basilisk.py
@@ -4,7 +4,7 @@ import requests
 from tqdm import tqdm
 
 if len(sys.argv) != 2:
-    print('You must enter the model name as a parameter, e.g.: download_model.py 124M')
+    print('You must enter the model name as a parameter')
     sys.exit(1)
 
 model = sys.argv[1]
@@ -12,17 +12,16 @@ model = sys.argv[1]
 subdir = os.path.join('models', model)
 if not os.path.exists(subdir):
     os.makedirs(subdir)
-subdir = subdir.replace('\\','/') # needed for Windows
+subdir = subdir.replace('\\','/') # Windows needs this because they use \ instead of /
 
 for filename in ['checkpoint','encoder.json','hparams.json','model.ckpt.data-00000-of-00001', 'model.ckpt.index', 'model.ckpt.meta', 'vocab.bpe']:
 
-    r = requests.get("https://openaipublic.blob.core.windows.net/gpt-2/" + subdir + "/" + filename, stream=True)
+    r = requests.get("https://openaipublic.blob.core.windows.net/gpt-2/" + subdir + "/" + filename, stream=True) # Literally use GPT-2 lol
 
     with open(os.path.join(subdir, filename), 'wb') as f:
         file_size = int(r.headers["content-length"])
-        chunk_size = 1000
+        chunk_size = 1000 # Chunk size is 1000 because Etherent max packet size is 1500 bytes
         with tqdm(ncols=100, desc="Fetching " + filename, total=file_size, unit_scale=True) as pbar:
-            # 1k for chunk_size, since Ethernet packet size is around 1500 bytes
             for chunk in r.iter_content(chunk_size=chunk_size):
                 f.write(chunk)
                 pbar.update(chunk_size)
