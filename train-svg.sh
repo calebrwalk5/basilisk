6c3f365c834a977e04fef5e44bd31b91afe06304
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2022-09-08 19:40:40 -0400

    Add files via upload

--- train-svg.sh
+++ train-svg.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo Encoding SVG...
-python3 src/encode.py training/training-data.svg training/output.npz
+cp ./training/training-data.svg ./training/training-svg.txt
+python3 src/encode.py training/training-svg.txt training/output.npz
 
 echo Training on the SVG...
 python3 src/train.py --dataset training/output.npz
