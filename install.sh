2ed2b98ce1bb3a24243f5e165012e82f5aa33a71
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2022-09-09 12:13:31 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo pip3 install -r requirements.txt
-sudo pip3 install tensorflow
-sudo pip3 install numpy
-python3 basilisk.py 124M
+sudo pip3 install tensorflow          # YES I'M THAT LAZY
+sudo pip3 install numpy               # another example of laziness
+python3 basilisk.py 124M              # download and use OpenAI's 124M
 touch training/training-data.txt
