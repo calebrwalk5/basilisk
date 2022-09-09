a0fa4b9d9a5a9c73e20df21aca49e1d0a1321653
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2022-09-09 12:16:56 -0400

    Update encoder.py

--- src/encoder.py
+++ src/encoder.py
@@ -99,7 +99,7 @@ class Encoder:
         bpe_tokens = []
         for token in re.findall(self.pat, text):
             token = ''.join(self.byte_encoder[b] for b in token.encode('utf-8'))
-            bpe_tokens.extend(self.encoder[bpe_token] for bpe_token in self.bpe(token).split(' '))
+            bpe_tokens.extend(self.encoder[bpe_token] for bpe_token in self.bpe(token).split(' '))      # nobody will ever read this and it's sad
         return bpe_tokens
 
     def decode(self, tokens):
