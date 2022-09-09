3a25f7b9bfb5f436a99c1a4ce94e534eb04ba2c4
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2022-09-09 12:15:35 -0400

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo Encoding...
-python3 src/encode.py training/training-data.txt training/output.npz
+python3 src/encode.py training/training-data.txt training/output.npz    # yep, that's me. lazy
 
 echo Training...
 python3 src/train.py --dataset training/output.npz
