ec6ece478ebd93ebf1c1709342254a9e5cec4e6c
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2023-01-20 09:27:47 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 sudo python -m pip install -r requirements.txt
 sudo python -m pip install tensorflow           # YES I'M THAT LAZY
 sudo python -m pip install numpy                # another example of laziness
-python3 basilisk.py 124M                        # download and use OpenAI's 124M
+python3 basilisk.py 124M                        # download and use OpenAI's model with 124 million paramaters 
